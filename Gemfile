d939febc56e569dc410372b966f819ae85574ea3
Ryuji Nakamura <nakamura@e-m-g.jp>
2020-07-30 09:51:26 +0900

    add gem rails-controller-testing

--- Gemfile
+++ Gemfile
@@ -66,6 +66,7 @@ group :test do
   gem 'chromedriver-helper'
   gem 'shoulda-matchers'
   gem 'launchy'
+  gem 'rails-controller-testing'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- spec/requests/surplus_lands_request_spec.rb
+++ spec/requests/surplus_lands_request_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "SurplusLands", type: :request do
 
   describe "GET /index" do
     it "returns http success" do
-      get "/surplus_lands/index"
+      get root_path
       expect(response).to have_http_status(:success)
     end
   end
