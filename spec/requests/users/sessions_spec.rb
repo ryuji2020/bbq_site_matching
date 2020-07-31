b4de8a983469bb3af532444b7cced9bd3065b8e4
Ryuji Nakamura <nakamura@e-m-g.jp>
2020-07-31 17:31:51 +0900

    create users/session test

--- spec/requests/users/sessions_spec.rb
+++ spec/requests/users/sessions_spec.rb
@@ -1,10 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe "Users::Sessions", type: :request do
-  describe "GET /users/sessions" do
-    it "works! (now write some real specs)" do
-      get users_sessions_index_path
+  describe "GET /users/sign_in" do
+    it "returns http success" do
+      get new_user_session_path
       expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'POST /users/sign_in' do
+    let(:user) { create(:user) }
+
+    before(:each) { post user_session_path, params: user_params }
+
+    context 'login with valid information' do
+      let(:user_params) { { user: { email: user.email, password: user.password } } }
+
+      it 'successful login' do
+        expect(response).to redirect_to root_path
+      end
+    end
+
+    context 'login with invalid information' do
+      let(:user_params) { { user: { email: 'wrong@address', password: 'wrongpass' } } }
+
+      it 'login fails' do
+        expect(response).to render_template 'new'
+      end
     end
   end
 end
