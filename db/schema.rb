e1d1ee7c18ed619ab42557a8dd31355493212998
Ryuji Nakamura <nakamura@e-m-g.jp>
2020-08-01 00:12:59 +0900

    genarate model SurplusLand

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_30_093711) do
+ActiveRecord::Schema.define(version: 2020_07_31_144935) do
+
+  create_table "surplus_lands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title"
+    t.integer "price"
+    t.string "state"
+    t.text "description"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["state"], name: "index_surplus_lands_on_state"
+    t.index ["user_id"], name: "index_surplus_lands_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +39,5 @@ ActiveRecord::Schema.define(version: 2020_07_30_093711) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "surplus_lands", "users"
 end
---
+++ spec/factories/surplus_lands.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :surplus_land do
+    title { "MyString" }
+    price { 1 }
+    state { "MyString" }
+    description { "MyText" }
+    user { nil }
+  end
+end
