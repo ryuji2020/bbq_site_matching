72d7c204560d898ac7194b845186c326a1e16e41
Ryuji Nakamura <nakamura@e-m-g.jp>
2020-08-04 22:12:40 +0900

    change variable name value->file in custom.js

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,8 +1,8 @@
 //============== ファイルサイズが5MB以上のときにアラートを出す =================
 $(function() {
   $('#surplus_land_images').bind('change', function() {
-    $.each(this.files, function(index, value) {
-      if (value.size/1024/1024 > 5) {
+    $.each(this.files, function(index, file) {
+      if (file.size/1024/1024 > 5) {
         alert(`${index+1}枚目のファイルサイズが5MBを超えています`);
       }
     });
