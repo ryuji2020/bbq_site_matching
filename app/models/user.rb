35fea226d1fcf84bebf006acbc1dc31e7aa26fb1
Ryuji Nakamura <nakamura@e-m-g.jp>
2020-08-07 08:43:32 +0900

    make model-spec of Like

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :surplus_lands, dependent: :destroy
+  has_many :likes, dependent: :destroy
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, format: { with: VALID_EMAIL_REGEX }
--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+  let(:other_user) { create(:user) }
+  let(:surplus_land) { create(:surplus_land, :with_prefecture, user: user) }
+
+  before(:each) { create(:like, surplus_land: surplus_land, user: user) }
+
+  describe 'validations' do
+    it { should validate_presence_of(:user_id) }
+
+    it { should validate_presence_of(:surplus_land_id) }
+
+    it 'surplus_land_id is uniqueness within the scope of user_id' do
+      invalid_like = Like.new(surplus_land_id: surplus_land.id, user_id: user.id)
+      expect(invalid_like).not_to be_valid
+      valid_like = Like.new(surplus_land_id: surplus_land.id, user_id: other_user.id)
+      expect(valid_like).to be_valid
+    end
+  end
+
+  describe 'association' do
+    it { should belong_to :user }
+
+    it { should belong_to :surplus_land }
+
+    it 'destroyed dependent to user' do
+      expect { user.destroy }.to change(Like, :count).by(-1)
+    end
+
+    it 'destroyed dependent to surplus_land' do
+      expect { surplus_land.destroy }.to change(Like, :count).by(-1)
+    end
+  end
 end
