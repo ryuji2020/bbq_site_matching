f4cd3bf7f5691d8298df06a931dc27e65d9d9449
Ryuji Nakamura <nakamura@e-m-g.jp>
2020-08-10 20:35:24 +0900

    use slick and display image to surplus_land/show-page

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -8,3 +8,20 @@ $(function() {
     });
   });
 });
+//============== surplus_land show-pageの画像表示 slickを使用 ==================
+$(function() {
+  $('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
+  $('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    centerMode: true,
+    focusOnSelect: true
+  });
+});
