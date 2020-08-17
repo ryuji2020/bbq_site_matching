71d4442591e476d03e5eafd11984ec2601d2bb22
Ryuji Nakamura <nakamura@e-m-g.jp>
2020-08-17 19:25:18 +0900

    create rooms#create-action and rooms/show-page

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,8 +2,22 @@ class RoomsController < ApplicationController
   before_action :authenticate_user!
 
   def show
+    @room = Room.find(params[:id])
+    @messages = Message.where(room_id: @room.id)
   end
 
   def create
+    surplus_land = SurplusLand.find(params[:surplus_land_id])
+    room = surplus_land.rooms.build(visitor_id: current_user.id)
+    if room.save
+      current_user.send_messages.create(
+        content: '行ってみたいです！',
+        receiver_id: surplus_land.user.id,
+        room_id: room.id
+      )
+      redirect_to room
+    else
+      redirect_to surplus_land
+    end
   end
 end
