311fcbfb7db39a78cf5b77f4bd0ee6e29a0bdc88
Ryuji Nakamura <nakamura@e-m-g.jp>
2020-08-18 11:02:41 +0900

    create the request-spec of rooms

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,8 +1,9 @@
 class RoomsController < ApplicationController
   before_action :authenticate_user!
+  before_action :correct_user, only: [:show]
+  before_action :only_visit_user, only: [:create]
 
   def show
-    @room = Room.find(params[:id])
     surplus_land = @room.surplus_land
     if request.referrer == surplus_land_url(surplus_land) && current_user != surplus_land.user
       current_user.send_messages.create(
@@ -15,17 +16,32 @@ class RoomsController < ApplicationController
   end
 
   def create
-    surplus_land = SurplusLand.find(params[:surplus_land_id])
-    room = surplus_land.rooms.build(visitor_id: current_user.id)
+    room = @surplus_land.rooms.build(visitor_id: current_user.id)
     if room.save
       current_user.send_messages.create(
         content: '行ってみたいです！',
-        receiver_id: surplus_land.user_id,
+        receiver_id: @surplus_land.user_id,
         room_id: room.id
       )
       redirect_to room
     else
-      redirect_to surplus_land
+      redirect_to @surplus_land
     end
+  end
+
+  private
+
+  #before_action
+
+  def correct_user
+    @room = Room.find(params[:id])
+    unless current_user?(@room.owner) || current_user?(@room.visitor)
+      redirect_to root_url
+    end
+  end
+
+  def only_visit_user
+    @surplus_land = SurplusLand.find(params[:surplus_land_id])
+    redirect_to root_url if current_user?(@surplus_land.user)
   end
 end
--- spec/requests/rooms_request_spec.rb
+++ spec/requests/rooms_request_spec.rb
@@ -1,5 +1,85 @@
 require 'rails_helper'
 
 RSpec.describe "Rooms", type: :request do
+  let(:surplus_land) { create(:surplus_land, :with_prefecture, :with_user) }
+  let(:visitor) { create(:user) }
 
+  describe 'GET /rooms/:id' do
+    let(:room) { create(:room, surplus_land: surplus_land, visitor: visitor) }
+
+    context 'when not user signed in' do
+      it 'ridirect to sign-in-page' do
+        get room_path(room)
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+
+    context 'when user signed in' do
+      context 'sign in with unrelated user' do
+        let(:other_user) { create(:user) }
+
+        before(:each) { sign_in other_user }
+
+        it 'can not enter the talk room' do
+          get room_path(room)
+          expect(response).to redirect_to root_url
+        end
+      end
+
+      context 'sign in with surplus_land.user(room.owner)' do
+        before(:each) { sign_in room.owner }
+
+        it 'returs http success' do
+          get room_path(room)
+          expect(response).to have_http_status :success
+        end
+      end
+
+      context 'sign in with visitor' do
+        before(:each) { sign_in room.visitor }
+
+        it 'returns http success' do
+          get room_path(room)
+          expect(response).to have_http_status :success
+        end
+      end
+    end
+  end
+
+  describe 'POST /rooms' do
+    let(:params) { { surplus_land_id: surplus_land.id } }
+
+    context 'when not user signed in' do
+      it 'can not create talk-room' do
+        expect do
+          post rooms_path, params: params
+        end.not_to change(Room, :count)
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+
+    context 'when user signed in' do
+      context 'sign in with surplus_land.user' do
+        before(:each) { sign_in surplus_land.user }
+
+        it 'can not create talk-room(can not apply to own surplus_land)' do
+          expect do
+            post rooms_path, params: params
+          end.not_to change(Room, :count)
+          expect(response).to redirect_to root_url
+        end
+      end
+
+      context 'sign in with other than surplus_land.user' do
+        before(:each) { sign_in visitor }
+
+        it 'created the talk-room(success to apply)' do
+          expect do
+            post rooms_path, params: params
+          end.to change(Room, :count).by(1)
+          expect(response).to redirect_to room_path(Room.last)
+        end
+      end
+    end
+  end
 end
