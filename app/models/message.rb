32cc7dd0011c112fce14d5049d397838f3e0f2cc
Ryuji Nakamura <nakamura@e-m-g.jp>
2020-08-19 17:09:42 +0900

    create-notification-method of message and add column to notifications message_id

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,21 @@
 class Message < ApplicationRecord
   belongs_to :room
   belongs_to :sender, class_name: 'User'
+  has_many :notifications, dependent: :destroy
 
   validates :content, presence: true, length: { maximum: 140 }
   validates :room_id, presence: true
   validates :sender_id, presence: true
+
+  # 通知作成メソッド
+  def create_notification(current_user, room)
+    visited_id = current_user == room.visitor ? room.owner.id : room.visitor_id
+    notification = current_user.active_notifications.build(
+      visited_id: visited_id,
+      room_id: room.id,
+      message_id: id,
+      action: 'message'
+    )
+    notification.save if notification.valid?
+  end
 end
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -3,6 +3,7 @@ class Notification < ApplicationRecord
   belongs_to :visited, class_name: 'User'
   belongs_to :surplus_land, optional: true
   belongs_to :room, optional: true
+  belongs_to :message, optional: true
 
   default_scope -> { order(created_at: :desc) }
 end
