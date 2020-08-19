789b8ab0f29ad2e5ce8cf918e38313557ffeeb05
Ryuji Nakamura <nakamura@e-m-g.jp>
2020-08-19 18:36:41 +0900

    create notification of message

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,4 +1,12 @@
 class NotificationsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
+    @notifications = current_user.passive_notifications.where.not(visitor_id: current_user.id).includes(:visitor)
+    if unchecked_notifications = @notifications.where(checked: false)
+      unchecked_notifications.each do |notification|
+        notification.update_attribute(:checked, true)
+      end
+    end
   end
 end
