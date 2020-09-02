073da4293a095ec12d5fee38cdee47d89c3d4279
Ryuji Nakamura <nakamura@e-m-g.jp>
2020-09-02 11:19:45 +0000

    modify credential and config/deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ set :log_level, :debug
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml", 'config/master.key'
+append :linked_files, 'config/master.key'
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "vendor/bundle"
