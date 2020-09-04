76270e57987aa8542fec0123c984e36c03188bdd
Ryuji Nakamura <nakamura@e-m-g.jp>
2020-09-04 07:44:10 +0000

    modify deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -42,37 +42,9 @@ set :keep_releases, 5
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
 
+after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
-  desc 'Restart application'
   task :restart do
     invoke 'unicorn:restart'
-  end
-
-  desc 'Create database'
-  task :db_create do
-    on roles(:db) do |host|
-      with rails_env: fetch(:rails_env) do
-        within current_path do
-          execute :bundle, :exec, :rails, 'db:create'
-        end
-      end
-    end
-  end
-
-  desc 'Run seed'
-  task :seed do
-    on roles(:app) do
-      with rails_env: fetch(:rails_env) do
-        within current_path do
-          execute :bundle, :exec, :rake, 'db:seed'
-        end
-      end
-    end
-  end
-
-  after :publishing, :restart
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do; end
   end
 end
